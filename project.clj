14f1bcaeef45a689bde2b355ddc7803778a1d7e4
Phil Hagelberg <technomancy@gmail.com>
2010-11-11 21:28:38 -0800

    Bump version to 1.1.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject robert/hooke "1.0.2"
+(defproject robert/hooke "1.1.0-SNAPSHOT"
   :description "Hooke your functions!"
-  :dev-dependencies [[org.clojure/clojure "1.1.0"]
-                     [swank-clojure "1.2.1"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                     [swank-clojure "1.2.1"]]
+  :test-selectors {:default (complement :skip)})

1aec6c8e4dd6ae2b53889db4198da9d184770101
Phil Hagelberg <technomancy@gmail.com>
2011-06-30 10:21:33 -0700

    Release 1.1.2, now with Clojure 1.3 compatibility.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject robert/hooke "1.1.1"
+(defproject robert/hooke "1.1.2"
   :description "Hooke your functions!"
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-                     [swank-clojure "1.2.1"]]
+  :dev-dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                     #_[lein-multi "1.0.0"]]
+  ;; TODO: lein-multi is not working here
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]}
   :test-selectors {:default (complement :skip)})

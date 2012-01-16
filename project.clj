580d844ffe357a8eb1bb1d7f0f67423d58269f38
Phil Hagelberg <technomancy@gmail.com>
2012-01-16 13:33:27 -0800

    Release 1.1.3.

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
-(defproject robert/hooke "1.1.2"
+(defproject robert/hooke "1.1.3"
   :description "Hooke your functions!"
-  :dev-dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                     #_[lein-multi "1.0.0"]]
-  ;; TODO: lein-multi is not working here
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]}
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]]
   :test-selectors {:default (complement :skip)})

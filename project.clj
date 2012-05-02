0dda14496d6d78d5e7b71731434519c63a2306ee
Phil Hagelberg <technomancy@gmail.com>
2012-05-02 15:14:35 -0700

    Add license to project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject robert/hooke "1.1.3"
   :description "Hooke your functions!"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :test-selectors {:default (complement :skip)})

4cf676f635b9859082b3ca3d654bd55f7f609ef7
Phil Hagelberg <technomancy@gmail.com>
2012-05-17 14:24:38 -0700

    Update for compatibility with Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject robert/hooke "1.1.3"
   :description "Hooke your functions!"
-  :dev-dependencies [[org.clojure/clojure "1.4.0"]]
+  :url "https://github.com/technomancy/robert-hooke"
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
